3cfe45bf1f8f3f30b0ab2ce748f4d3a9af369733
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2019-12-07 13:27:37 +0100

    Update code.

--- persistence/src/main/java/com/oreilly/dao/JdbcOfficerDAO.java
+++ persistence/src/main/java/com/oreilly/dao/JdbcOfficerDAO.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
